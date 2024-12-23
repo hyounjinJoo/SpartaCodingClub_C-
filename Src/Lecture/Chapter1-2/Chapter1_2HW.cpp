9cc1ae8c354ea810d89a0af258d5b235108d88a5
HyounJinDev <hyounjin.dev@gmail.com>
2024-12-23 13:32:28 +0900

    [Update] 강의 Chapter1-2 과제 1 구현완료

--- Src/Lecture/Chapter1-2/Chapter1_2HW.cpp
+++ Src/Lecture/Chapter1-2/Chapter1_2HW.cpp
@@ -1,7 +1,18 @@
-﻿#include "Chapter1_2HW.h"
+﻿#include "../../Common/CommonFunction.h"
+
+#include "Chapter1_2HW.h"
 
 int Run()
 {
+	Battery NewBattery;
+	Print("Initial charge: ", false);
+	std::cout << NewBattery.getCharge() << "%\n";
+
+	NewBattery.useBattery();
+	NewBattery.useBattery();
+	NewBattery.chargeBattery();
+	NewBattery.useBattery();
+
 	return 0;
 }
 
@@ -10,4 +21,45 @@ int Chapter1_2HWMain()
 	int Result = Run();
 
 	return Result;
+}
+
+Battery::Battery(int initialCharge)
+	: charge(0)
+	, usingCost(5)
+	, chargingPower(7)
+{
+	charge = initialCharge;
+}
+
+void Battery::useBattery()
+{
+	charge -= usingCost;
+	if(0 > charge)
+	{
+		charge = 0;
+	}
+
+	Print("Battery used. Current charge: ", false);
+	PrintCurrentCharge();
+}
+
+void Battery::chargeBattery()
+{
+	charge += chargingPower;
+	if(100 < charge)
+	{
+		charge = 100;
+	}
+
+	Print("Battery charged. Current charge: ", false);
+	PrintCurrentCharge();
+}
+
+void Battery::PrintCurrentCharge(bool bNeedENDL) const
+{
+	std::cout << charge << "%";
+	if (bNeedENDL)
+	{
+		std::cout << "\n";
+	}
 }
--- Src/Lecture/Chapter1-2/Chapter1_2HW.h
+++ Src/Lecture/Chapter1-2/Chapter1_2HW.h
@@ -2,3 +2,27 @@
 
 int Chapter1_2HWMain();
 int Run();
+
+class Battery
+{
+public:
+	Battery(int initialCharge = 100);
+	virtual ~Battery(){};
+
+public:
+	int getCharge() const
+	{
+		return charge;
+	}
+
+	void useBattery();
+	void chargeBattery();
+
+private:
+	void PrintCurrentCharge(bool bNeedENDL = true) const;
+
+private:
+	int charge;
+	int usingCost;
+	int chargingPower;
+};
