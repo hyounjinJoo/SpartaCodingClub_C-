1e4be68a636f299ee19d48b2a64f383838b2bba2
HyounJinDev <hyounjin.dev@gmail.com>
2024-12-23 14:28:18 +0900

    [Update] 1-3 강의 과제 구현 완료

--- Src/Lecture/Chapter1-3/Chapter1_3HW.cpp
+++ Src/Lecture/Chapter1-3/Chapter1_3HW.cpp
@@ -1,7 +1,38 @@
 ﻿#include "Chapter1_3HW.h"
 
+#include "../../Common/CommonFunction.h"
+
 int Chapter1_3HWMain()
 {
+	Adventure* adventure[3];
+	adventure[0] = new Warrior();
+	adventure[1] = new Mage();
+	adventure[2] = new Archer();
+
+	for(int Number = 0; Number < 3; ++Number)
+	{
+		adventure[Number]->useSkill();
+	}
+
+	for (int Number = 0; Number < 3; ++Number)
+	{
+		delete adventure[Number];
+	}
 
 	return 0;
+}
+
+void Warrior::useSkill()
+{
+	Print("Warrior uses Slash!");
+}
+
+void Mage::useSkill()
+{
+	Print("Mage casts Fireball!");
+}
+
+void Archer::useSkill()
+{
+	Print("Archer shoots an Arrow!");
 }
--- Src/Lecture/Chapter1-3/Chapter1_3HW.h
+++ Src/Lecture/Chapter1-3/Chapter1_3HW.h
@@ -1,3 +1,26 @@
 ﻿#pragma once
 
 int Chapter1_3HWMain();
+
+class Adventure
+{
+public:
+	Adventure(){};
+	virtual ~Adventure(){};
+
+public:
+	virtual void useSkill() = 0;
+};
+
+class Warrior final : public Adventure
+{
+	virtual void useSkill() override;
+};
+class Mage final : public Adventure
+{
+	virtual void useSkill() override;
+};
+class Archer final : public Adventure
+{
+	virtual void useSkill() override;
+};
